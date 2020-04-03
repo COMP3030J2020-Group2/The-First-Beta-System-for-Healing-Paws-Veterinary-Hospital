a806e15997a5ee16838bcaeb44853f9ee6062fde
Wurinigong <904284330@qq.com>
2020-04-03 18:48:28 +0800

    write corresponding route and Form class of sign up

--- HospitalSystem/HospitalSystem/systemapp/forms.py
+++ HospitalSystem/HospitalSystem/systemapp/forms.py
@@ -1,1 +1,10 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class SignupForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField('Repeat Password', validators=[DataRequired()])
+	submit = SubmitField('Register')
--- HospitalSystem/HospitalSystem/systemapp/routes.py
+++ HospitalSystem/HospitalSystem/systemapp/routes.py
@@ -7,3 +7,23 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = SignupForm()
+	if form.validate_on_submit():
+		customer = Customer.query.filter(Customer.email == form.email.data).first()
+		if customer:
+			return render_template('signup.html', title='Register a new user',form=form, emailerror = "Email already been used by other people")
+
+		customer = Customer.query.filter(Customer.username == form.username.data).first()
+		if customer:
+			return render_template('signup.html', title='Register a new user',form=form, usernameerror = "Username already been used by other people")
+
+		passw_hash = generate_password_hash(form.password.data)
+		customer = Customer(username=form.username.data, email=form.email.data, password_hash=passw_hash)
+		db.session.add(customer)
+		db.session.commit()
+		session["USERNAME"] = customer.username
+		return render_template('after_login_page.html')
+	return render_template('signup.html', title='Register a new user', form=form)
