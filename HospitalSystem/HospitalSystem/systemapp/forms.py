7585dc5a09052c626590ab4cc8bc37d16de61565
Wurinigong <904284330@qq.com>
2020-04-04 15:10:40 +0800

    Completed Pet signup and Modified form style

--- HospitalSystem/HospitalSystem/systemapp/forms.py
+++ HospitalSystem/HospitalSystem/systemapp/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField,SelectField
 from wtforms.validators import DataRequired
 
 class SignupForm(FlaskForm):
@@ -7,4 +7,10 @@ class SignupForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	password2 = PasswordField('Repeat Password', validators=[DataRequired()])
+	submit = SubmitField('Register')
+
+
+class PetSignUpForm(FlaskForm):
+	petname = StringField('Pet Name', validators=[DataRequired()])
+	type = SelectField('Type',coerce=int, choices=[(0, 'Please Choice'),(1, 'Cat'), (2, 'Dog')],default = 0)
 	submit = SubmitField('Register')
--- HospitalSystem/HospitalSystem/systemapp/routes.py
+++ HospitalSystem/HospitalSystem/systemapp/routes.py
@@ -1,8 +1,8 @@
 from systemapp import app,db
-from flask import render_template, session, request, jsonify
+from flask import render_template, session, request, jsonify,redirect,url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-from systemapp.forms import SignupForm
-from systemapp.models import Customer
+from systemapp.forms import SignupForm,PetSignUpForm
+from systemapp.models import Customer, Pet
 
 @app.route('/')
 
@@ -30,6 +30,29 @@ def signup():
 		session["USERNAME"] = customer.username
 		return render_template('after_login_page.html')
 	return render_template('signup.html', title='Register a new user', form=form)
+
+@app.route('/pet_signup', methods=['GET', 'POST'])
+def pet_signup():
+	form = PetSignUpForm()
+	if not session.get("USERNAME") is None:
+		if form.validate_on_submit():
+			customer = Customer.query.filter(Customer.username == session.get("USERNAME")).first()
+			pet = Pet.query.filter(Pet.name == form.petname.data and Pet.owner_id == customer.id).first()
+			if pet:
+				return render_template('pet_signup.html', information='Same Name!', form=form)
+			else:
+				if form.type.data == 0:
+					return render_template('pet_signup.html', information='Choose a type', form=form)
+				else:
+					new_pet = Pet(name=form.petname.data,type=form.type.data,owner = customer)
+					db.session.add(new_pet)
+					db.session.commit()
+			return render_template('pet_signup.html', information='Successful!', form=form)
+		return render_template('pet_signup.html', information='Register a new pet', form=form)
+	else:
+		return redirect(url_for('signup'))
+
+
 
 @app.route('/checkuser', methods=['POST'])
 def check_username():
